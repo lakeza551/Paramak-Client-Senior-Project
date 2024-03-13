288cf9b67e08f054cb6f38fc2e13c4f6cd1c42ce
Paramak <lakeza551@gmail.com>
2024-03-13 19:04:10 +0700

    adjust css

--- src/Record.jsx
+++ src/Record.jsx
@@ -7,65 +7,73 @@ function Record(props) {
     const params = new URLSearchParams(search)
     const token = params.get('token')
     const [patientData, setPatientData] = useState(null)
-    useEffect(async () => {
-        try {
-            const res = await fetch(`/patient_data?token=${token}`)
-            const { data } = await res.json()
-            setPatientData(data)
-        } catch (error) {
-            console.log(error)
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res = await fetch(`https://medrecords.sc.su.ac.th/patient_data?token=${token}`)
+                const { data } = await res.json()
+                data.records.reverse()
+                setPatientData(data)
+            } catch (error) {
+                console.log(error)
+            }
         }
+        getData()
     }, [])
     if(patientData === null)
         return <h1>กำลังโหลดข้อมูล</h1>
     return (
         <div className="page-container">
-            <h1 className='patient-pid'>รหัสประจำตัวประชาชน {patientData.pid}</h1>
+            <h1 className='patient-pid'>รหัสประจำตัวประชาชน <span style={{color: "rgb(6, 99, 165)"}}>{patientData.pid}</span></h1>
             {patientData.records.map(record => {
                 return (
                     <div className="table-container">
                         <label className="table-date">วันที่ {record.date}</label>
                         <table>
-                            <tr>
-                                <th style={{
-                                    width: '30%'
-                                }}>
-                                    Name
-                                </th>
-                                <th style={{
-                                    width: '50%'
-                                }}>
-                                    Dosage
-                                </th>
-                                <th style={{
-                                    width: '20%'
-                                }}>
-                                    Quantity
-                                </th>
-                            </tr>
-                            {record.medicines.map(med => {
-                                return (
-                                    <>
-                                        <tr>
-                                            <td style={{
-                                                width: '30%'
-                                            }}>
-                                                {med.trade_name} {med.dosage_form} {med.dose}
-                                            </td>
-                                            <td style={{
-                                                width: '50%'
-                                            }}>
-                                                {med.frequency}
-                                            </td>
-                                            <td style={{
-                                                width: '20%'
-                                            }}>
-                                                {med.quantity}
-                                            </td>
-                                        </tr>
-                                    </>
-                                )
-                            })}
+                            <thead>
+                                <tr>
+                                    <th style={{
+                                        width: '30%'
+                                    }}>
+                                        Name
+                                    </th>
+                                    <th style={{
+                                        width: '50%'
+                                    }}>
+                                        Dosage
+                                    </th>
+                                    <th style={{
+                                        width: '20%'
+                                    }}>
+                                        Quantity
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {record.medicines.map(med => {
+                                    return (
+                                        <>
+                                            <tr>
+                                                <td style={{
+                                                    width: '30%'
+                                                }}>
+                                                    {med.trade_name} {med.dosage_form} {med.dose}
+                                                </td>
+                                                <td style={{
+                                                    width: '50%'
+                                                }}>
+                                                    {med.frequency}
+                                                </td>
+                                                <td style={{
+                                                    width: '20%'
+                                                }}>
+                                                    {med.quantity}
+                                                </td>
+                                            </tr>
+                                        </>
+                                    )
+                                })}
+                            </tbody>
                         </table>
                     </div>
                 )
